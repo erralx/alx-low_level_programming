a03672de5d5557a301e933310af0ec5a9f47b6c4
Amine <amine.erramy.alx@gmail.com>
2023-09-12 12:50:29 -0700

    3-

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower: Checks for lowercase char
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
