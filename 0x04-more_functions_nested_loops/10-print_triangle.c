e9373ad291774b66899642c57fd53eee4382528d
Amine <amine.erramy.alx@gmail.com>
2023-09-15 06:17:18 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size; j >= 1; j--)
+		{
+			if (i >= j)
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
