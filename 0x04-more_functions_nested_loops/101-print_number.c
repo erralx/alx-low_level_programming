525756000a2aa3438bebfa05ce18b6655937b4a0
Amine <amine.erramy.alx@gmail.com>
2023-09-15 08:16:15 -0700

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	int n_recur = n;
+
+	if (n < 0)
+	{
+		n = -n;
+		n_recur = n;
+		_putchar('-');
+	}
+
+	n_recur = n_recur / 10;
+
+	if (n_recur != 0)
+	{
+		print_number(n_recur);
+	}
+
+	_putchar(n % 10 + '0');
+}
