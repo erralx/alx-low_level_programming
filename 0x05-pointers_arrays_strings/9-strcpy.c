139c7acff15b4f77db604391f6e23f77d4d4b3e5
Amine <amine.erramy.alx@gmail.com>
2023-09-18 18:34:41 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,38 +11,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int n;
 	int i;
 
-	n = string_length(src);
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	for (; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+	dest[i++] = '\0';
 
 	return (dest);
-}
-
-/**
- * string_length - finds the length of a string.
- * @str: string whose length to find.
- *
- * Return: length of str.
- */
-int string_length(char *str)
-{
-	int c = 0;
-
-	while (*(str + c) != '\0')
-	{
-		c++;
-	}
-
-	return (c);
 }
