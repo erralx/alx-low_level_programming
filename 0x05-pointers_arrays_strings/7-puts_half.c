e4595239d7f718503d93ab34b16963ed1672c39f
Amine <amine.erramy.alx@gmail.com>
2023-09-18 18:09:27 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string whose half to print
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	if (len % 2 != 0)
+	{
+		len = (len + 1) / 2;
+	}
+	else
+	{
+		len /= 2;
+	}
+
+	while (len < i)
+	{
+		len++;
+		_putchar(*(str + len - 1));
+	}
+
+	_putchar('\n');
+}
