ef7a71adc8255cb2f04a0417f734c1499526faaf
Amine <amine.erramy.alx@gmail.com>
2023-10-05 08:31:51 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,10 @@
 
 /**
  * main - prints the name of the program, followed by a new line
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: Always 0
  */
 int main(int argc, char **argv)
 {
