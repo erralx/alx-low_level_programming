17ca44f0460193b2ad85f0075adcae0dc5cd2ddd
Amine <amine.erramy.alx@gmail.com>
2023-10-06 08:51:26 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,7 @@
 #include <limits.h>
 
+unsigned int contains(char *str, char c);
+
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string to search
@@ -10,22 +12,33 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len = 0, i;
+	unsigned int len = 0, i, j, res;
 
-	for (; *accept != '\0'; accept++)
+	for (i = 0;; i++)
 	{
-		for (i = 0;; i++)
+		if (accept[i] == '\0')
 		{
-			if (s[i] == '\0')
+			break;
+		}
+
+		for (j = 0;; j++)
+		{
+			if (s[j] == '\0')
 			{
 				return (0);
 			}
 
-			if (s[i] == *accept)
+			if (s[j] == accept[i])
 			{
-				if (i + 1 > len)
-					len = i + 1;
+				if (j + 1 > len)
+					len = j + 1;
 				break;
+			}
+
+			res = contains(accept, s[j]);
+			if (res == 0)
+			{
+				return (0);
 			}
 
 			continue;
@@ -33,4 +46,18 @@ unsigned int _strspn(char *s, char *accept)
 	}
 
 	return (len);
+}
+
+/**
+ *
+*/
+unsigned int contains(char *str, char c)
+{
+	for (; *str != '\0'; str++)
+	{
+		if (*str == c)
+			return (1);
+	}
+
+	return (0);
 }
