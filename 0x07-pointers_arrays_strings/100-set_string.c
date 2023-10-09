aecec93c55fb0e0f2dee050dd676a35604e658f8
Amine <amine.erramy.alx@gmail.com>
2023-10-09 07:26:51 -0700

    100-set_string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,17 +1,3 @@
-/* #include <stdlib.h> */
-
-/* int _strlen(char *s)
-{
-	int i, len;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		len++;
-	}
-
-	return (len);
-} */
-
 /**
  * set_string - sets the value of a pointer to a char
  * @s: pointer whose value to set
@@ -19,9 +5,5 @@
  */
 void set_string(char **s, char *to)
 {
-	/* int s_new_len = _strlen(to);
-	int s_new_bytes = s_new_len + 1;
-	char *s_new = malloc(s_new_bytes); */
-
 	*s = to;
 }
