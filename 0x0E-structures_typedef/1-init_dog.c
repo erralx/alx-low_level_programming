c397e9d1f052b3e5e2180838a882d691c1330f6b
Amine <amine.erramy.alx@gmail.com>
2023-10-10 12:13:51 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,13 @@
 
 /**
  * init_dog - intializes a dog structure with provided members
- * @dog: pointer to structure to initialize
+ * @d: pointer to structure to initialize
  * @name: name of dog
  * @age: age of dog
  * @owner: name of dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{	
+{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
