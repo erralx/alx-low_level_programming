12a886def8c8955393985856227a058c0d2dbd35
Amine <amine.erramy.alx@gmail.com>
2023-10-10 16:44:39 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -45,6 +45,9 @@ void _strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
