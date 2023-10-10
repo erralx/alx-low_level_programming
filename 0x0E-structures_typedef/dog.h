b2e03dad6c14f105d5dec0746f0d8145754f93fe
Amine <amine.erramy.alx@gmail.com>
2023-10-10 12:01:24 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
