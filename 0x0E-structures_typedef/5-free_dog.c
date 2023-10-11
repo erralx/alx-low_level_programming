ef548317b78904a7222d9a233b8773eae4bba177
Amine <amine.erramy.alx@gmail.com>
2023-10-10 17:13:13 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,7 +7,12 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d->name != NULL)
+		free(d->name);
+
+	if (d->owner != NULL)
+		free(d->owner);
+
+	if (d != NULL)
+		free(d);
 }
