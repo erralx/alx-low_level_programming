afe92ffc7d51c954a72cd7ff3b580fdc86170498
Amine <amine.erramy.alx@gmail.com>
2023-10-11 06:12:56 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - Prints a name
  * @name: name to print
@@ -5,5 +7,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		return;
+
 	f(name);
 }
