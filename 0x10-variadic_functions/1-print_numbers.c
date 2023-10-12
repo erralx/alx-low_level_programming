feae8220cae4dd633ea970ff90e2fc8e05431fdb
Amine <amine.erramy.alx@gmail.com>
2023-10-12 04:00:45 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,17 +11,13 @@ void print_numbers(char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	ap = va_start(ap, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, int));
-		if (i != n - 1)
-		{
-			if (separator != NULL)
-				printf("%s", separator);
-			printf(" ");
-		}
+		printf("%i", va_arg(ap, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
 	}
 
 	printf("\n");
