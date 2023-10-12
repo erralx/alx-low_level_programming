a721cc49cab1719db5233b6766dc16b5f042d238
Amine <amine.erramy.alx@gmail.com>
2023-10-12 06:23:10 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct printer_t - printer struct
+ * @specifier: format of argument to print
+ * @f: printer function
+ */
+struct printer_t
+{
+	char specifier;
+	void (*f)(va_list ap);
+};
 
 #endif /* MAIN_H */
