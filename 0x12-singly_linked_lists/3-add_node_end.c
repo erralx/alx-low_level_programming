a785740d6f81340128f3c35e4b2d430095b7524c
Amine <amine.erramy.alx@gmail.com>
2023-10-19 13:34:07 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,6 +10,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
+	list_t *node_i;
 
 
 	if (head == NULL || str == NULL)
@@ -17,6 +18,8 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	new->next = NULL;
+	new->str = strdup(str);
+	new->len = strlen(str);
 
 	if ((*head) == NULL)
 	{
@@ -24,15 +27,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		while ((*head)->next != NULL)
+		node_i = *head;
+		while (node_i->next != NULL)
 		{
-			head = &((*head)->next);
+			node_i = node_i->next;
 		}
 
-		(*head)->next = new;
+		node_i->next = new;
 	}
 
-	new->str = strdup(str);
-	new->len = strlen(str);
 	return (new);
 }
