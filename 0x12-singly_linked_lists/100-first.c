7369624fd88b495d17f6ff3eab9970bb86f30179
Amine <amine.erramy.alx@gmail.com>
2023-10-19 22:40:32 +0100

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,9 @@
 
 void __attribute__((constructor)) first();
 
+/**
+ * first - runs before main is executed
+ */
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
