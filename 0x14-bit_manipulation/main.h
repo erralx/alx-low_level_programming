1814b376c660a25ddbaf2c5603b9ea6667a0d2f1
Amine <amine.erramy.alx@gmail.com>
2023-10-27 14:42:58 +0100

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,9 +4,9 @@
 #include <stddef.h>
 
 int _pow_recursion(int x, int y);
-
+int _putchar(char c);
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
-
