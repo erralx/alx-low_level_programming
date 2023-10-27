97415339b286c673d47ec728752b824e0d8af7b8
Amine <amine.erramy.alx@gmail.com>
2023-10-27 14:19:56 +0100

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,39 @@
 #include "main.h"
 
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ * - there is one or more chars in the string b that is not 0 or 1
+ * - b is NULL
+*/
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int uint = 0;
-    int i, pow;
+	unsigned int uint = 0;
+	int i, pow;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+		return (0);
 
-    for (i = 0; b[i] != '\0'; i++)
-        ;
-    i--;
+	for (i = 0; b[i] != '\0'; i++)
+		;
+	i--;
 
-    for (pow = 0; i >= 0; i--, pow++)
-    {
-        if (b[i] == '0')
-            continue;
+	for (pow = 0; i >= 0; i--, pow++)
+	{
+		if (b[i] == '0')
+			continue;
 
-        if (b[i] == '1')
-        {
-            uint += _pow_recursion(2, pow);
-            continue;
-        }
+		if (b[i] == '1')
+		{
+			uint += _pow_recursion(2, pow);
+			continue;
+		}
 
-        return (0);
-    }
+		return (0);
+	}
 
-    return (uint);
+	return (uint);
 }
 
 /**
@@ -38,11 +45,11 @@ unsigned int binary_to_uint(const char *b)
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return (-1);
+	if (y < 0)
+		return (-1);
 
-    if (y == 0)
-        return (1);
+	if (y == 0)
+		return (1);
 
-    return (x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,3 +9,4 @@ int _pow_recursion(int x, int y);
 unsigned int binary_to_uint(const char *b);
 
 #endif /* MAIN_H */
+
